2de672ba400970a30644538fc7f98759a6817054
rileyhagerstromumaine <riley.ahegrstrom@maine.edu>
2021-12-13 14:36:45 -0500

    Removed unsed functions not related to development testing
    Added Comments in Main and over fucntion definitions
    
    Spell checked comments

--- main.c
+++ main.c
@@ -1,18 +1,17 @@
+// Pulse Oximeter Capstone Design Project
+// Riley Hagerstrom and Cully Richard
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <math.h>
+
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 #include "hardware/gpio.h"
 #include "hardware/adc.h"
-
-#include "sys_fn.h"
-#include "sys_i2c.h"
-#include "sys_time.h"
-#include "dev_mcp4728.h"
 
 #include "ssd1306.h"
 
@@ -39,11 +38,12 @@ const uint display_scl_pin = 19;
 const uint red_led_pin = 16;
 const uint ir_led_pin = 17;
 
+//Calibration constats for spO2 calculation
 static const int cal_f_a = 0;
 static const int cal_f_b = 40;
 static const int cal_f_c = 77;
 
-
+//Global variables to hold AC/DC RED and IR values
 int sampledIR[SAMPLE_SIZE];
 int ACDC_IR[2];
 int sampledR[SAMPLE_SIZE];
@@ -53,23 +53,27 @@ float spO2_avg_arr[SAMPLE_SIZE_ROLL_AVG];
 float spO2_avg_arr_temp[SAMPLE_SIZE_ROLL_AVG];
 
 void setup_gpios(void);
+
 void sampleIR();
 void sampleR();
+
 void printArray(int arr[], int n);
+void printfloatArray(float arr[], int n);
+
 void getACDC(int arr[], int n, int color);
 float find_spO2(int AC_IR, int DC_IR, int AC_R, int DC_R);
-void setup_gpios(void);
+
 int getIRVal(void);
 int getRVal(void);
 
 
 
 int main() {
-    //Initilize Serial port
+    //Initialize Serial port
     stdio_init_all();
 
     //Sleep to wait for user to load serial moniter
-    sleep_ms(10000);
+    sleep_ms(1000);
     //Setup display and onboard led variables
     printf("configuring pins...\n");
     setup_gpios();
@@ -77,21 +81,22 @@ int main() {
     //Ports
     i2c_inst_t *i2c = i2c1;
 
-    //Initilize Display
+    //Initialize Display
     ssd1306_t disp;
     disp.external_vcc=false;
     ssd1306_init(&disp, 128, 64, SSD1306_WRITE_ADDRESS, i2c1);
     ssd1306_clear(&disp);
 
-    //Initiliza spO2 global varibles
+    //Initialize spO2 global variables
     float spO2 = 0;
 
-    //Initilize spO2 average
+    //Initialize spO2 average
     float spO2_avg  = 0;
 
+    //Initialize Counter
     int i;
 
-    for(int i = 0; i < SAMPLE_SIZE_ROLL_AVG; i++){
+    for(i = 0; i < SAMPLE_SIZE_ROLL_AVG; i++){
         spO2_avg_arr[i] = 0;
     }
 
@@ -101,6 +106,7 @@ int main() {
         sampleIR();
         sampleR();
 
+        //Print Sampled IR and RED Values to Serial Moniter
         printf("Sampled IR Values\n");
         printArray(sampledIR,SAMPLE_SIZE);
         printf("Sampled RED Values\n");
@@ -110,38 +116,34 @@ int main() {
         getACDC(sampledIR,SAMPLE_SIZE,1);
         getACDC(sampledR,SAMPLE_SIZE,0);
 
+        //Print AC/DC IR and RED Values to Serial Moniter
         printf("AC DC IR Values\n");
         printArray(ACDC_IR,2);
         printf("AC DC RED Values\n");
         printArray(ACDC_R,2);
 
-        //Calculate spO2
+        //Calculate spO2 and Print to Serial Moniter
         spO2 = find_spO2(ACDC_IR[0],ACDC_IR[1],ACDC_R[0],ACDC_R[1]);
         printf("Calculated SpO2 Value %f \n", spO2);
 
+        //Update the spO2 rolling average array
         for (i = 0; i < SAMPLE_SIZE_ROLL_AVG - 1; i++){
             spO2_avg_arr[i] = spO2_avg_arr[i+1];
         }
         spO2_avg_arr[SAMPLE_SIZE_ROLL_AVG-1] = spO2;
 
-        
-
-        //Update the spO2 rolling average array
-        //update_spO2(spO2);
+        //Print Updated Arrary to Moniter
         printfloatArray(spO2_avg_arr,SAMPLE_SIZE_ROLL_AVG);
 
-
-
-        //Calculate the rollig average
+        //Calculate the rolling average
         float sum = 0;
         for(int i = 0; i < SAMPLE_SIZE_ROLL_AVG; i++){
             sum = sum + spO2_avg_arr[i];
         }
         spO2_avg = sum / SAMPLE_SIZE_ROLL_AVG;
 
-    
-
-        printf("jumping to animation...\n");
+        //Update display with rolling average
+        printf("Updating Display...\n");
         char buffer[15];
         
         snprintf(buffer,15,"%.0f",spO2_avg);
@@ -150,20 +152,13 @@ int main() {
         ssd1306_clear(&disp);
   
         sleep_ms(25);
-        
-    
         memset(buffer, 0, 10);
-    
         sleep_ms(25);
-
-
     }
-    
-
     return 0;
 }
 
-
+//----------Setup and Display Functions----------
 void setup_gpios(void) {
 
     //Setup Devboard onboard GPIO
@@ -188,14 +183,11 @@ void setup_gpios(void) {
     adc_init();
     adc_set_temp_sensor_enabled(true);
     adc_select_input(0);
-
 }
-
 
 
 void animationUpdate(char word[]) //Write a string to display
 {
-
     ssd1306_t disp;
     disp.external_vcc=false;
     ssd1306_init(&disp, 128, 64, 0x3C, i2c1);
@@ -204,7 +196,6 @@ void animationUpdate(char word[]) //Write a string to display
     ssd1306_draw_string(&disp, 8, 24, 2, word);
     ssd1306_show(&disp);
     ssd1306_clear(&disp);
-    
 }
 
 
@@ -247,55 +238,15 @@ float find_spO2(int AC_IR, int DC_IR, int AC_R, int DC_R) // Find the R value
     return spO2;
 }
 
-void update_spO2(float spO2){
-    //temp arrary to copy over values
-    float spO2_avg_temp[SAMPLE_SIZE_ROLL_AVG];
-    //copy over temp values 
-    for(int i = 1; i < SAMPLE_SIZE_ROLL_AVG; i++){
-        spO2_avg_temp[i-1] = spO2_avg_arr[i];
-    }
-
-    spO2_avg_temp[SAMPLE_SIZE_ROLL_AVG] = spO2;
-
-    printf("print temp array");
-    printfloatArray(spO2_avg_temp,SAMPLE_SIZE_ROLL_AVG);
-
-    for(int i = 0; i < SAMPLE_SIZE_ROLL_AVG; i++){
-        spO2_avg_arr[i] = spO2_avg_temp[i];
-    }
-
-   
-}
-
-
-
-float rmsValue(int arr[], int n) //find the RMS value given an arry of intigers
-{
-    int square = 0;
-    float mean = 0.0, root = 0.0;
- 
-    // Calculate square.
-    for (int i = 0; i < n; i++) {
-        square += pow(arr[i], 2);
-    }
- 
-    // Calculate Mean.
-    mean = (square / (float)(n));
- 
-    // Calculate Root.
-    root = sqrt(mean);
- 
-    return root;
-}
 
 void mathRoutine() //Display ACDC of a dummy int array
 
 {
     //Dummy array to hold sampled values
     int arr[] = {604, 636, 763, 452, 864, 969, 29, 262, 910, 941, 142, 745, 102, 218, 309, 616, 613, 594, 419, 917, 487, 539, 230, 929, 275, 662, 270, 114, 479, 670, 640, 726, 887, 559, 973, 983, 895, 754, 720, 432, 360, 112, 397, 669, 172, 694, 888, 566, 696, 666, 409, 290, 810, 374, 851, 779, 780, 939, 653, 667, 959, 388, 556, 971, 956, 503, 189, 731, 963, 809, 862, 173, 383, 120, 404, 992, 744, 161, 260, 537, 416, 312, 522, 254, 289, 258, 575, 131, 947, 330, 894, 264, 504, 69, 512, 347, 174, 447, 40, 836};
-    //Pointer to pass by refernce the resules of getACDC
+    //Pointer to pass by refernce the results of get ACDC
     int *p;
-    //Size of arry used in arrary processing
+    //Size of array used in arrary processing
     int n = sizeof(arr)/sizeof(arr[0]);
 
     //Store AC DC to p
@@ -305,6 +256,7 @@ void mathRoutine() //Display ACDC of a dummy int array
     printArray(p,2);
 }
 
+//Utility Function prints initger array
 void printArray(int arr[], int n)
 {   
     printf("[");
@@ -314,6 +266,7 @@ void printArray(int arr[], int n)
     printf("]\n");
 }
 
+//Utility Function prints float array
 void printfloatArray(float arr[], int n)
 {   
     printf("[");
@@ -323,6 +276,7 @@ void printfloatArray(float arr[], int n)
     printf("]\n");
 }
 
+//Utility Function makes random initiger, was used in development to simulate DAC sample
 int makeRandom(int lower, int upper)
 {
     int num = (rand() %(upper - lower + 1)) + lower;
@@ -371,7 +325,7 @@ void R_OFF()
     //printf("RED off\n");
 }
 
-int getIRVal(void) //find the intesity of IR
+int getIRVal(void) //find the intensity of IR
 { 
     IR_ON(); //turn on IR LED
     busy_wait_ms(SAMPLE_WAIT);
@@ -382,7 +336,7 @@ int getIRVal(void) //find the intesity of IR
     return IR_VAL;
 }
 
-int getAVal(void) //find the intesity of Ambient
+int getAVal(void) //find the intensity of Ambient
 { 
     IR_OFF(); //IR LED OFF
     busy_wait_ms(SAMPLE_WAIT);
@@ -393,7 +347,7 @@ int getAVal(void) //find the intesity of Ambient
     return A_VAL;
 }
 
-int getRVal(void) //find the insensity of RED
+int getRVal(void) //find the intensity of RED
 { 
     R_ON(); //turn 
     busy_wait_ms(SAMPLE_WAIT);
@@ -404,5 +358,3 @@ int getRVal(void) //find the insensity of RED
     return R_VAL;
 }
 
-//--------------DAC I2C Functions---------------
-//int reg_write()
